d39ccebfb97b82494a6d386d45114698cadf84c5
Dulce Ariadna Martínez González <dulce.martinez.2024@outlook.com>
2024-04-30 13:18:39 -0600

    Agregar archivos de proyecto.

---
+++ MVCCrud/Controllers/TablaController.cs
@@ -0,0 +1,66 @@
+﻿using MVCCrud.Models;
+using MVCCrud.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MVCCrud.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (CrudEntities db = new CrudEntities())
+            {
+                lst = (from d in db.tabla
+                       select new ListTablaViewModel
+                       {
+                           ID = d.ID,
+                           Nombre = d.Nombre,
+                           Correo = d.Correo,
+                           Fecha_nacimiento = (DateTime)d.Fecha_nacimiento
+                       }).ToList();
+            }
+
+            return View(lst);
+
+        }
+
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid) 
+                {
+                    using (CrudEntities db = new CrudEntities()) 
+                    {
+                        var oTabla = new tabla();
+                        oTabla.Correo = model.Correo;
+                        oTabla.Fecha_nacimiento = model.Fecha_nacimiento;
+                        oTabla.Nombre = model.Nombre;
+
+                        db.tabla.Add(oTabla);
+                        db.SaveChanges();
+                    }
+                }
+                return Redirect("Tabla/");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ MVCCrud/Models/ViewModels/TablaViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrud.Models.ViewModels
+{
+    public class TablaViewModel
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Nombre")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Correo electronico")]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Correo { get; set; }
+        [Required]
+        [Display(Name = "Fecha de nacimiento")]
+        public DateTime Fecha_nacimiento { get; set; }
+    }
+}
